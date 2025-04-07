0b1808f83c8b745198f501a17e494f38ad6cc59a
the Institute <info@ntari.org>
2025-04-07 00:52:28 -0400

    Create App.js
    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FallacyTrainer from './components/FallacyTrainer';
+
+function App() {
+  return (
+    <div className="App">
+      <FallacyTrainer />
+    </div>
+  );
+}
+
+export default App;
